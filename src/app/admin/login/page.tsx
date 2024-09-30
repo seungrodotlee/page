f012224707a85e4e5e65d22826c30248104036fd
이승로 <seungrodotlee@gmail.com>
2024-09-30 22:54:01 +0900

    test. test in login page

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,9 +1,14 @@
+import { operationEnv } from "@/@shared/utils/env";
 import { loginWithGithub } from "./actions";
 
 export default function LoginPage() {
   return (
     <form>
       <button formAction={loginWithGithub}>Log in</button>
+      <p>
+        {process.env.NODE_ENV}
+        {operationEnv}
+      </p>
     </form>
   );
 }
